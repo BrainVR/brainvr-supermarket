f6c5fd58153ad02f92087eed5da3a049e73ed0d0
hejtmy <hejtmy@gmail.com>
2019-11-05 19:32:58 +0100

    Slight changes in the position getters

--- R/super-getters.R
+++ R/super-getters.R
@@ -1,24 +1,52 @@
-get_position_data_object <- function(obj, object_name = NULL, i_cycle = NULL, i_task = NULL){
+#' Returns position data for picking up a specific object in given trial
+#'
+#' @param obj loaded supermarket data
+#' @param object_name object name as it appears in the experiment log
+#' @param i_task if there are more objects of the same name picked up, this defines which cycle you are interested in
+#'
+#' @return navr object with the object path (from previous item to the current item)
+#' @export
+#'
+#' @examples
+get_position_pickup <- function(obj, object_name = NULL, i_cycle = NULL){
   exp_log <- get_experiment_log(obj)
-  if(!is.null(object_name)){
-    i_row <- which(exp_log$ObjectName == object_name)
-  } else {
-    i_row <- which(exp_log$TestCycle == i_cycle)[i_task]
+  exp_log$pickup_order <- 1:nrow(exp_log) #for time getting
+  obj_log <- exp_log[exp_log$ObjectName == object_name, ]
+  if(nrow(obj_log) > 1){
+    if(is.null(i_cycle)){
+      warning("There were ", nrow(obj_log), "items ", object_name, " picked up. Specify i_cycle to select one")
+      return(NULL)
+    }
+    obj_log <- obj_log[obj_log$TestCycle == i_cycle, ]
+    if(nrow(obj_log)!=1){
+      warning("There were ", nrow(obj_log), "items ", object_name, " picked up in the cycle. Author needs to fix this function." )
+      return(NULL)
+    }
   }
-  if(i_row == 1){
+  # TODO - this needs to reflect tasks whcih are first in each cycle, not jsut first task.- This information is in the result log
+  if(obj_log$pickup_order == 1){
     start_time <- get_log(obj)$timestamp[1]
   } else {
-    start_time <- exp_log$Time[i_row-1]
+    start_time <- exp_log$Time[obj_log$pickup_order-1] #previous item pickup time
   }
-  end_time <- exp_log$Time[i_row]
+  end_time <- obj_log$Time
   pos <- get_position_timewindow(obj, start_time, end_time)
   return(pos)
 }
 
-get_task_position <- function(obj, task_items = NULL){
+#' Returns position during trial
+#'
+#' @param obj supermarket object
+#' @param i_trial
+#'
+#' @return navr position object
+#' @export
+#'
+#' @examples
+get_position_trial <- function(obj, i_trial){
   exp_log <- get_experiment_log(obj)
-  i_rows <- which(exp_log$TaskItems == task_items)
-  if(task_items == 3){
+  i_rows <- which(exp_log$TestCycle == i_trial)
+  if(i_trial == 1){
     start_time <- get_log(obj)$timestamp[1]
   } else {
     start_time <- exp_log$Time[i_rows[1] - 1]
