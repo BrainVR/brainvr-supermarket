daf552fb306b74caa8f5cbb6da457999ce48f0fc
hejtmy <hejtmy@gmail.com>
2019-11-06 19:07:45 +0100

    Adds void therem to the plot and properly scaled background

--- R/super-visualisations.R
+++ R/super-visualisations.R
@@ -10,14 +10,14 @@
 #'
 #' @examples
 plot_supermarket_path <- function(obj, background = FALSE, background_type = "categories", custom_background = NULL, ...){
-  g <- ggplot()
+  g <- ggplot() + geom_navr_limits(obj) ## this is just to set the generic
   if(background){
     if(background_type == "categories") background_path <- BACKGROUND_CATEGORIES_PATH
     if(background_type == "plain") background_path <- PLAIN_CATEGORIES_PATH
     if(!is.null(custom_background)) background_path <- custom_background
     g <- g + geom_navr_background(background_path, xlim=AREA_BOUNDARIES$x, ylim = AREA_BOUNDARIES$y)
   }
-  g <- g + geom_navr_path(obj, ...)
+  g <- g + geom_navr_path(obj, ...) + theme_void()
   return(g)
 }
 
