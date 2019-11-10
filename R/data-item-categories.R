71b0ff69a551bdbf89c4b5653e2205a18087b6d0
hejtmy <hejtmy@gmail.com>
2019-11-10 23:36:57 +0100

    Adds description of the intem categories data

---
+++ R/data-item-categories.R
@@ -0,0 +1,13 @@
+#' Item categories encoding table
+#'
+#' This dataframe serves the purpose to convert between language versions
+#' as well as codument categorization of individual objects
+#' @format A data.frame with 212 items and 5 variables:
+#' \describe {
+#'    \item{ID}{ID of the item. In english, but ultimately convertable}
+#'    \item{CZ}{Czech name of the item. UTF-8 encoded}
+#'    \item{EN}{English common name of the item. uTF-8 encoded}
+#'    \item{CZR}{}
+#'    \item{Category}{name of the category ofg the item in english}
+#' }
+"item_categories"
