5c4ec9e439df4eed37db3643cf788165bbd03dab
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-09-11 13:14:33 +0200

    Adds pkgdown site

--- R/data-item-categories.R
+++ R/data-item-categories.R
@@ -3,7 +3,7 @@
 #' This dataframe serves the purpose to categorize of individual objects
 #'
 #' @format A data.frame with 212 items and 5 variables:
-#' \describe {
+#' \describe{
 #'    \item{ID}{ID of the item. In english, but ultimately convertable}
 #'    \item{Category}{name of the category ofg the item in english}
 #' }
--- R/data-item-translations.R
+++ R/data-item-translations.R
@@ -5,7 +5,7 @@
 #' Language options at this point include "CZ", "CZR" and "EN"
 #'
 #' @format A data.frame with 212 items and 5 variables:
-#' \describe {
+#' \describe{
 #'    \item{ID}{ID of the item}
 #'    \item{CZ}{Czech name of the item. UTF-8 encoded}
 #'    \item{EN}{English common name of the item. uTF-8 encoded}
